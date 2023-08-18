df1f6d48bc2a65935523b39c3007ee5a7759d4ad
Adam <73994535+adamtuft@users.noreply.github.com>
2023-08-18 10:05:26 +0100

    Bump version to 0.3.0

--- ckernel/__init__.py
+++ ckernel/__init__.py
@@ -1,7 +1,7 @@
 """ckernel module __init__.py"""
 from __future__ import annotations
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from . import async_kernel, autocompile_kernel, base_kernel, resource
 
